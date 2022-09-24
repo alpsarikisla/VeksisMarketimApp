0ce71eaf9f8af30087f3693c8d831d9ee43d76b1
Alp Sarıkışla <50963484+alpsarikisla@users.noreply.github.com>
2022-09-24 13:21:22 +0300

    DataAccessLayerAcildi

---
+++ DataAccessLayer/ConnectionStrings.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer
+{
+    public class ConnectionStrings
+    {
+        static string DataSource = "mssql08.turhost.com";
+        static string InitialCatalog = "veksisc2_Veksis_Marketim_DB";
+        static string UserID = "vk_Admn";
+        static string Password = "&Pw8pr99?*";
+
+        public static string ConStrLive = String.Format("Data Source={0}; Initial Catalog={1}; Persist Security Info=True;User ID={2}; Password={3}", DataSource, InitialCatalog, UserID, Password);
+
+        public static string ConStrTest = @"Data Source=DESKTOP-6NIP90O\SQLEXPRESS; Initial Catalog=Veksis_Market_DB; Integrated Security=True";
+    }
+}
---
+++ DataAccessLayer/DataModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer
+{
+    public class DataModel
+    {
+        SqlConnection con; SqlCommand cmd;
+
+       public DataModel()
+        {
+            con = new SqlConnection(ConnectionStrings.ConStrLive);
+            cmd = con.CreateCommand();
+        }
+
+        #region Kullanıcı Metotları
+
+
+
+        #endregion
+    }
+}
